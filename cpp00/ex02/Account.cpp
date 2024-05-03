424112a300e3e62b1a9f2bda78606dfe2263652e
Dani X <danixidev@gmail.com>
2024-05-03 19:46:16 +0200

    cpp00-ex02 base

---
+++ cpp00/ex02/Account.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dximenez <dximenez@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/03 19:28:33 by dximenez          #+#    #+#             */
+/*   Updated: 2024/05/03 19:36:32 by dximenez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+Account::Account(int initial_deposit)
+{}
+
+Account::~Account(void)
+{}
+
+int	Account::getNbAccounts(void)
+{}
+
+int	Account::getTotalAmount(void)
+{}
+
+int	Account::getNbDeposits(void)
+{}
+
+int	Account::getNbWithdrawals(void)
+{}
+
+void	Account::displayAccountsInfos(void)
+{}
+
+void	Account::makeDeposit(int deposit)
+{}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{}
+
+int	Account::checkAmount(void) const
+{}
+
+void	Account::displayStatus( void ) const
+{}
+
+void	Account::_displayTimestamp( void )
+{}
