95c5b7d17b079de41d47f9096c1f87da199bc005
Dani X <danixidev@gmail.com>
2024-06-18 11:59:40 +0200

    Robotomy form

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -6,25 +6,33 @@
 /*   By: dximenez <dximenez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 18:20:34 by dximenez          #+#    #+#             */
-/*   Updated: 2024/06/18 10:59:28 by dximenez         ###   ########.fr       */
+/*   Updated: 2024/06/18 11:41:52 by dximenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 #include "AForm.hpp"
 #include "ShubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
 
 int	main()
 {
+	srand(time(NULL));
 	try
 	{
 		Bureaucrat b("Paco", 1);
 		ShubberyCreationForm f1("Shub Form");
+		RobotomyRequestForm f2("Robot Form");
 
 		b.signForm(f1);
 		b.executeForm(f1);
+
+		std::cout << f1 << std::endl << std::endl;
 		
-		std::cout << f1 << std::endl;
+		b.signForm(f2);
+		b.executeForm(f2);
+		
+		std::cout << f2 << std::endl << std::endl;
 	}
 	catch (std::exception &e)
 	{
