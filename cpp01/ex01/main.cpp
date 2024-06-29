84d258f1ceefd24fece77f3f7f09aa33efc62506
Dani X <danixidev@gmail.com>
2024-06-29 14:03:36 +0200

    Fix cpp01 ex01

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -6,37 +6,20 @@
 /*   By: dximenez <dximenez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 22:09:27 by dximenez          #+#    #+#             */
-/*   Updated: 2024/05/04 15:00:28 by dximenez         ###   ########.fr       */
+/*   Updated: 2024/06/29 14:03:11 by dximenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-static bool	is_valid_number(std::string s)
-{
-	for (size_t i = 0; i < s.length(); i++)
-		if (!isdigit(s[i]))
-			return (false);
-	return (true);
-}
-
 int main(void)
 {
-	std::string	input;
-	std::string	name;
+	int			number = 5;
+	std::string	name = "Z";
 
-	do
-	{
-		std::cout << "Number of zombies in horde:\t";
-		std::cin >> input;
-	} while (!is_valid_number(input));
+	Zombie *zombies = zombieHorde(number, name);
 
-	std::cout << "Name of the zombies:\t";
-	std::cin >> name;
-
-	Zombie *zombies = zombieHorde(atoi(input.c_str()), name);
-
-	for (int i = 0; i < atoi(input.c_str()); i++)
+	for (int i = 0; i < number; i++)
 		zombies[i].announce();
 
 	delete[] zombies;
--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: dximenez <dximenez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 14:10:05 by dximenez          #+#    #+#             */
-/*   Updated: 2024/05/04 14:12:41 by dximenez         ###   ########.fr       */
+/*   Updated: 2024/06/29 14:02:52 by dximenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 
 Zombie* zombieHorde(int N, std::string name )
 {
+	if (N < 0)
+	{
+		std::cout << "Invalid number of zomboies" << std::endl;
+		return (NULL);
+	}
 	Zombie	*zombie = new Zombie[N];
 
 	for (int i = 0; i < N; i++)
