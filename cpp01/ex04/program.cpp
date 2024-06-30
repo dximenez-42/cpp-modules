565b8b2f2ac7065efd7d8cc4c3d60f2486d8e229
Dani <danixidev@gmail.com>
2024-06-30 15:05:50 +0200

    Fix cpp01 ex04

--- cpp01/ex04/program.cpp
+++ cpp01/ex04/program.cpp
@@ -6,7 +6,7 @@
 /*   By: dximenez <dximenez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 19:39:25 by dximenez          #+#    #+#             */
-/*   Updated: 2024/06/27 11:43:21 by dximenez         ###   ########.fr       */
+/*   Updated: 2024/06/30 15:03:56 by dximenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void	program(std::string filename, std::string s1, std::string s2)
 	std::ifstream	infile;
 	std::ofstream	outfile;
 	std::string		str;
+	std::string		ret;
 
 	infile.open(filename.c_str());
 	outfile.open((filename + ".replace").c_str(), std::ofstream::out | std::ofstream::trunc);
@@ -26,14 +27,25 @@ void	program(std::string filename, std::string s1, std::string s2)
 	if (infile.is_open())
 		std::getline(infile, str, '\0');
 
-	int	i;
-	while (s1 != s2 && (i = str.find(s1)) != -1)
+	if (s1.empty() || s2.empty())
 	{
-		str.erase(i, s1.length());
-		str.insert(i, s2);
+		outfile << str;
+		infile.close();
+		outfile.close();
+		return ;
 	}
+	
+	long			i = 0;
+	while ((i = str.find(s1)) != -1)
+	{
+		ret.append(str, 0, i);
+		ret.append(s2);
+		str = str.substr(i + s1.length());
+	}
+	if (str.empty() == false)
+		ret.append(str);
 
-	outfile << str;
+	outfile << ret;
 	infile.close();
 	outfile.close();
 }
